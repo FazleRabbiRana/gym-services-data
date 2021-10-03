b52a756da0ebd2e0e015798184f1d7d79026c103
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-10-04 04:18:12 +0600

    images and js file

---
+++ gym-services.js
@@ -0,0 +1,50 @@
+[
+	{
+		id: 1,
+		name: 'Personal Training',
+		price: '$50',
+		image: ''
+	},
+	{
+		id: 2,
+		name: 'Nutritional Counseling',
+		price: '$20',
+		image: ''
+	},
+	{
+		id: 3,
+		name: 'Body Massage',
+		price: '$60',
+		image: ''
+	},
+	{
+		id: 4,
+		name: 'Swim Lessons',
+		price: '$30',
+		image: ''
+	},
+	{
+		id: 5,
+		name: 'Outdoor Activities',
+		price: '$40',
+		image: ''
+	},
+	{
+		id: 6,
+		name: 'Yoga Training',
+		price: '$30',
+		image: ''
+	},
+	{
+		id: 7,
+		name: 'Medical Monitoring',
+		price: '$100',
+		image: ''
+	},
+	{
+		id: 8,
+		name: 'Competition Events',
+		price: '$150',
+		image: ''
+	},
+]
